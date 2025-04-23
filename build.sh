3d7f273b22ac68bbb52b9b7bcdd353bc974eb054
Davide Paro <dparo@outlook.it>
2025-04-23 12:58:14 +0200

    Fix build.sh script

--- build.sh
+++ build.sh
@@ -3,5 +3,11 @@
 
 set -eou pipefail
 
-pandoc -f markdown -s --embed-resources main.md -o main.html
+
+# Render Latex equations to SVG / PNG
+# pandoc -f markdown -s --embed-resources main.md -o main.html --webtex='https://latex.codecogs.com/svg.image?'
+pandoc -f markdown -s --embed-resources main.md -o main.html --webtex='https://latex.codecogs.com/png.image?'
+
+# Render Latex equations with mathml
+# pandoc -f markdown -s --embed-resources main.md -o main.html --mathml
 
